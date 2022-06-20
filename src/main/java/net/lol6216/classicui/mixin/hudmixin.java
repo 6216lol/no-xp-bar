2a4fd73a48c912475982e08e710f04f40c66ca4b
6216 <peacocklover69420@outlook.com>
2022-06-20 17:46:35 -0400

    Updated to 1.1: bugfixes and small aesthetic tweaks

--- src/main/java/net/lol6216/classicui/mixin/hudmixin.java
+++ src/main/java/net/lol6216/classicui/mixin/hudmixin.java
@@ -1,13 +1,11 @@
 package net.lol6216.classicui.mixin;
 
-import net.lol6216.classicui.start;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,10 +17,10 @@ public class hudmixin {
 		MinecraftClient client = MinecraftClient.getInstance();
 		TextRenderer renderer = client.textRenderer;
 		Object text =  Text.translatable("deathScreen.score").append(": ").append(Text.literal(Integer.toString(client.player.experienceLevel)));
-		renderer.drawWithShadow(m,(Text)text,3,3,16777215);
+		renderer.drawWithShadow(m,(Text)text,2,2,16777215);
 		callback.cancel();
 	}
-
+	/*
 	@Inject(at=@At("HEAD"), method="renderStatusBars")
 	public void statusbar1(MatrixStack m, CallbackInfo callback) {
 		m.push();
@@ -33,5 +31,15 @@ public class hudmixin {
 	public void statusbarreturn(MatrixStack m, CallbackInfo callback) {
 		m.pop();
 	}
-
+	*/
+	//Give it a more Legacy Console vibe
+	@Inject(at=@At("head"), method="renderHotbar")
+	private void hotbar1(float t, MatrixStack m, CallbackInfo callback) {
+		m.push();
+		m.translate(0,-6.8,0);
+	}
+	@Inject(at=@At("return"), method="renderHotbar")
+	private void hotbar2(float t, MatrixStack m, CallbackInfo callback) {
+		m.pop();
+	}
 }
